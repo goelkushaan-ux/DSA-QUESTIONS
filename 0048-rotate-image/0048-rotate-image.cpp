e0d584d343d41928505959d63cba36c995efbd22
goelkushaan-ux <goelkushaan@gmail.com>
2026-08-14 00:31:03 +0530

    Time: 0 ms (100%), Space: 10.2 MB (37.97%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i;j<matrix.size();j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size()/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.size()-j-1];
+                matrix[i][matrix.size()-j-1]=temp;
+            }
+        }
+
+
+    }
+};
