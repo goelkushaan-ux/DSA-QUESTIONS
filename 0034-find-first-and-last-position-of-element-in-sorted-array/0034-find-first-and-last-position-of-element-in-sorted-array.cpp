cad813f19d4708001a248102c01e424f306c3b89
goelkushaan-ux <goelkushaan@gmail.com>
2026-08-14 00:25:30 +0530

    Time: 0 ms (100%), Space: 17.5 MB (89.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,16 @@
 class Solution {
-
-    ///// find upper and lower values index
-
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans;
-        int a = -1, b = -1;
-        int left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] <= target) {
-                if (nums[mid] == target)
-                    a = mid;
-                left = mid + 1;
-            } else
-                right = mid - 1;
+        int a=-1, b=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                if(a==-1) a=i;
+                b=i;
+            }
         }
-        
-        left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] >= target) {
-                if (nums[mid] == target)
-                    b = mid;
-                right = mid - 1;
-            } else
-                left = mid + 1;
-        }
-        ans.push_back(b);
         ans.push_back(a);
+        ans.push_back(b);
         return ans;
     }
 };
