f5f7a98461959611112dd4123dfec2f1ef323514
Kushaan Goel <goelkushaan@gmail.com>
2026-09-06 15:11:32 +0530

    Time: 3 ms (45.27%), Space: 9.7 MB (48.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,13 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size())
             return false;
-        int a1[26] = {0}, a2[26] = {0};
+        int a1[26] = {0};
         for (int i = 0; i < s.size(); i++) {
             a1[s[i] - 'a']++;
-            a2[t[i] - 'a']++;
+            a1[t[i] - 'a']--;
         }
         for (int i = 0; i < 26; i++)
-            if (a1[i] != a2[i])
+            if (a1[i] != 0)
                 return false;
         return true;
     }
